d8d6de00fc4cc82b62af381f4dc9dcbc3d85fc2f
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-09-24 14:13:17 -0300

    lint: formata código

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,22 +1,22 @@
 const mongoose = require('mongoose')
 
-module.exports = function(){
-    const {
-        MONGODB_USER,
-        MONGODB_PASS,
-        MONGODB_SERVER,
-        MONGODB_DATABASE
-    }=process.env
-    mongoose.connect(`mongodb+srv://${MONGODB_USER}:${MONGODB_PASS}@${MONGODB_SERVER}/${MONGODB_DATABASE}?retryWrites=true&w=majority`,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
+module.exports = function () {
+  const { MONGODB_USER, MONGODB_PASS, MONGODB_SERVER, MONGODB_DATABASE } =
+    process.env
 
-    mongoose.connection.on('connected',()=>
-        console.log('** Mongoose concetado ao servidor remoto **')
-    )
+  mongoose.connect(
+    `mongodb+srv://${MONGODB_USER}:${MONGODB_PASS}@${MONGODB_SERVER}/${MONGODB_DATABASE}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
 
-    mongoose.connection.on('error',erro=>
-        console.error('***Mongoose: ERRO DE CONEXÃO. Causa: '+erro)
-    )
+  mongoose.connection.on('connected', () =>
+    console.log('** Mongoose concetado ao servidor remoto **')
+  )
+
+  mongoose.connection.on('error', erro =>
+    console.error('***Mongoose: ERRO DE CONEXÃO. Causa: ' + erro)
+  )
 }
