3311d1bf5b11b75696a552478ca83b1f47e156e4
ADRIEL BARBOSA BORGES <1091392123030@fatec-srv.local>
2022-10-24 19:48:48 -0300

    Alteração na CONTROLLER (glossary)

--- backend/controllers/glossary.js
+++ backend/controllers/glossary.js
@@ -1,83 +1,99 @@
 const Glossary = require('../models/Glossary')
 
-const controller = {} //objeto vazio
+const controller = {} // Objeto vazio
 
 /*
     Métodos de CRUD do controller
-
-    create: cria um novo usuário
-    retrieve: retorna todos os usuários cadastrados
-    retrieveOne: retorna um único usuário
-    update: atualiza os dados de um usuário
-    dele: exclui um usuário
+    create: cria um novo registro
+    retrieve: recupera todos os registros
+    rerieveOne: recupera um único registro
+    update: atualiza os dados de um registro
+    delete: exclui um registro
 */
 
 controller.create = async (req, res) => {
-  try {
-    const result = await Glossary.create(req.body)
-    //HTTP 201: Created
-    res.status(201).send(result)
-  } catch (error) {
-    console.error(error)
-    //HTTP 500: Internal Server Error
-    res.status(500).send(error)
-  }
+    try {
+        const result = await Glossary.create(req.body)
+        // HTTP 201: Created
+        res.status(200).send(result)
+
+    } catch (error) {
+        console.log(error)
+        // HTTP 500: Internal Server Error
+        res.status(500).send(error)
+    }
 }
 
 controller.retrieveAll = async (req, res) => {
-  try {
-    //find() sem parâmetros retorna todos os documentos da coleção
-    const result = await Glossary.find().populate('question')
-    //HTTP 200: OK (implícito)
-    res.send(result)
-  } catch (error) {
-    console.error(error)
-    //HTTP 500: Internal Server Error
-    res.status(500).send(error)
-  }
+    try {
+        // find() sem parametros retorna todos os documentos
+        const result = await Glossary.find().sort({ entry: 1 })
+
+        // HTTP 200: OK (implicito)
+        res.send(result)
+
+    } catch (error) {
+        console.log(error)
+        // HTTP 500: Internal Server Error
+        res.status(500).send(error)
+    }
 }
 
 controller.retrieveOne = async (req, res) => {
-  try {
-    const result = await Glossary.findById(req.params.id)
+    try {
+        const result = await Glossary.findById(req.params.id)
 
-    //HTTP 200: OK (implícito)
-    if (result) res.send(result) // Encontrou o documento
-    //HTTP 404: Not Found
-    else res.status(404).end() // Não encontrou
-  } catch (error) {
-    console.error(error)
-    //HTTP 500: Internal Server Error
-    res.status(500).send(error)
-  }
+        // HTTP 200: OK (implicito)
+        if (result) {
+            res.send(result)
+        } else {
+            // HTTP 404: Not Found
+            res.status(404).end()
+        }
+
+    } catch (error) {
+        console.log(error)
+        // HTTP 500: Internal Server Error
+        res.status(500).send(error)
+    }
 }
 
 controller.update = async (req, res) => {
-  try {
-    const result = await Glossary.findByIdAndUpdate(req.params.id, req.body, {returnDocument: 'after'})
+    try {
+        const result = await Glossary.findByIdAndUpdate(req.params.id, req.body)
 
-    //HTTP 204: No content
-    if (result) return res.status(204).end() // Encontrou e atualizou
-    else res.status(404).end() // Não encontrou
-  } catch (error) {
-    console.error(error)
-    //HTTP 500: Internal Server Error
-    res.status(500).send(error)
-  }
+        // HTTP 200: OK (implicito)
+        if (result) {
+            res.status(200).send(result)
+        } else {
+            // HTTP 404: Not Found
+            res.status(404).end()
+        }
+
+    } catch (error) {
+        console.log(error)
+        // HTTP 500: Internal Server Error
+        res.status(500).send(error)
+    }
 }
 
 controller.delete = async (req, res) => {
-  try {
-    const result = await Glossary.findByIdAndDelete(req.params.id)
+    try {
+        const result = await Glossary.findByIdAndDelete(req.params.id)
 
-    //HTTP 204: No content
-    if (result) return res.status(204).end() // Encontrou e atualizou
-    else res.status(404).end() // Não encontrou
-  } catch (error) {
-    console.error(error)
-    //HTTP 500: Internal Server Error
-    res.status(500).send(error)
-  }
+        // HTTP 200: OK (implicito)
+        if (result) {
+            res.status(200).send(result)
+        } else {
+            // HTTP 404: Not Found
+            res.status(404).end()
+        }
+
+    } catch (error) {
+        console.log(error)
+        // HTTP 500: Internal Server Error
+        res.status(500).send(error)
+    }
 }
 
 module.exports = controller
