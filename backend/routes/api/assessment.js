9d0c3d100d2fd191eea5261975e5945f2b6f319d
ABBorges1 <adrielbarborges@gmail.com>
2022-10-27 07:30:10 -0300

    27.10 - (ajuste de filtro, ordenação e índice)

--- backend/routes/api/assessment.js
+++ backend/routes/api/assessment.js
@@ -1,22 +1,18 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../../controllers/assessment');
+const express = require('express')
+const router = express.Router()
+const controller = require('../controllers/assessment')
 
-router.post('/', controller.create);
-router.get('/', controller.retrieveAll);
-router.get('/:id', controller.retrieveOne);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.delete);
+router.post('/', controller.create)
+router.get('/', controller.retrieveAll)
+router.get('/:id', controller.retrieveOne)
+router.patch('/:id', controller.update)
+router.delete('/:id', controller.delete)
 
-/**
- *  Rotas para answer
- */
+/* rotas para answer */
 router.post('/:assessment_id/answer', controller.createAnswer)
 router.get('/:assessment_id/answer', controller.retrieveAllAnswers)
-// router.get('/:assessment_id/answer/:id', controller.retrieveOneAnswer)
+router.get('/:assessment_id/answer/:id', controller.retrieveOneAnswer)
 router.patch('/:assessment_id/answer/:id', controller.updateAnswer)
- router.delete('/:assessment_id/answer/:id', controller.deleteAnswer)
+router.delete('/:assessment_id/answer/:id', controller.deleteAnswer)
 
-
-
-module.exports = router;
+module.exports = router
