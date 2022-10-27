61fc288db738588cb1660a98791caf09e7bc375a
ABBorges1 <adrielbarborges@gmail.com>
2022-10-27 07:33:52 -0300

    27.10 - (ajuste de filtro, ordenação e índice)

--- backend/models/Question.js
+++ backend/models/Question.js
@@ -1,36 +1,30 @@
 const mongoose = require('mongoose')
 
 const schema = mongoose.Schema({
-  // Campo de chave estrangeira para o model Criterion
-  criterion: {
-    type: mongoose.ObjectId, // Tipo especial
-    ref: 'Criterion', // Coleção referenciada
-    index: true,
-  },
-  order: {
-    type: Number,
-    required: true,
-    index: true
-  },
-  enunciation: {
-    type: String,
-    required: true,
-  },
-  glossary_refs: [
-    {
-      type: mongoose.ObjectId,
-      ref: 'Glossary',
+    criterion: {
+        type: mongoose.ObjectId,
+        ref: 'Criterion',
+        index: true
     },
-  ],
+    order: {
+        type: Number,
+        required: true,
+        index: true
+    },
+    enunciation: {
+        type: String,
+        required: true
+    },
+    glossary_refs: [{
+        type: mongoose.ObjectId,
+        ref: 'Glossary'
+    }]
 })
 
 /*
     Parâmetros de mongoose.model:
-    1º: o nome do model, para uso interno. Por convenção,
-        usa-se Inicial Maiúscula.
-    2º: a relação de campos do esquema (variável schema)
-    3º: o nome da collection no banco de dados (normalmente,
-        é o mesmo nome do model, mas pluralizado e com
-        inicial minúscula)
+    1º - Nome da model, para uso interno (convenção: primeira letra maiúscula e singular)
+    2º - Relação de campos do esquema (schema)
+    3º - Nome da collection no banco de dados (convenção: mesmo nome da model com letra inicial minúsculas e plural)
 */
 module.exports = mongoose.model('Question', schema, 'questions')
