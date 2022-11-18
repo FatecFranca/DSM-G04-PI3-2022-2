715af36f86d804f26c05675b2e0e9155af59cf99
Guilherme Teixeira Ais <73388069+guilhermeais@users.noreply.github.com>
2022-11-18 12:24:28 -0300

    feat: lida com o cors

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,12 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 
 const app = express();
 const { setupRoutes } = require('./routes');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
